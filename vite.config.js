44964aaf970dca03e8fecc7b865e3b459ba64550
Timothy <150438959+timothy0509@users.noreply.github.com>
2025-08-20 20:48:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/eta/",
+  // ensure client build outputs to dist
   build: {
-    ssr: "src/entry-server.jsx", // SSR entry
-    outDir: "dist-ssr"
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html"
+    }
   }
 });
